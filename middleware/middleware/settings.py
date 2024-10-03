c903a6be525775f8efcde59edb79e3c0a04fec8b
vishal24tuniki <vishal24.tuniki@gmail.com>
2024-10-03 20:18:24 +0530

    allowed hosts

--- middleware/middleware/settings.py
+++ middleware/middleware/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&$ukzax(28e+vnrzum@+!xjtw86zjg%+35%#dzd4omaj^p9n_m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
